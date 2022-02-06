b9bb8dcf448de144278778126f2c9f623cdd6586
Richard <43828146+RichardEpure@users.noreply.github.com>
2022-02-06 18:47:59 +0000

    Add new paths

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
+import os
 import pygame
 
 pygame.init()
 
 tile_size = (64, 64)
 
-path_style = 'resources/style.json'
-path_levels = 'resources/levels'
-path_images = 'resources/images'
+path_style = os.path.join('resources', 'style.json')
+path_levels = os.path.join('resources', 'levels')
+path_images = os.path.join('resources', 'images')
+path_logs = os.path.join('training', 'logs')
+path_PPO = os.path.join('training', 'saved models', 'PPO')
 
 if True:  # noqa: E402
     from gui import Window
